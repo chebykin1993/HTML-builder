5a22065b1a973acd3464ea4f169be15e9b42028e
chebykin1993 <chebykin1993@list.ru>
2023-05-04 17:45:31 +0300

    feat: complete read-file task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,1 +1,10 @@
-let hui
+const fs = require('fs')
+const path = require('path')
+const { stdin, stdout } = process
+
+const stream = fs.createReadStream( path.join('./01-read-file' ,'text.txt'), 'utf-8')
+let data = ''
+
+stream.on('data', chunk => data += chunk)
+stream.on('end', () => stdout.write(data))
+stream.on('error', error => stdout.write('Error', error.message))
