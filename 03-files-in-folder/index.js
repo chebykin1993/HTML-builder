52ac21a2ce2492e006424bdbec9030cf5b1464b6
chebykin1993 <chebykin1993@list.ru>
2023-05-05 15:17:01 +0300

    feat: done task3

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,17 @@
+const promises = require('fs/promises')
+const files = promises.readdir("./03-files-in-folder/secret-folder", {withFileTypes: true});
+const fs = require("fs")
+const path = require("path")
+
+files.then(files => {
+  for (let file of files) {
+      if (file.isFile()) {
+          let name = file.name
+          let ext = path.extname(`./03-files-in-folder/secret-folder/${name}`)
+          fs.stat(`./03-files-in-folder/secret-folder/${name}`,
+              (err, file) =>
+                  console.log(name.slice(0, name.indexOf(".")) + " - " + ext + " - " + file.size))
+      }
+  }
+})
+
