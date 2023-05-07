fb867bd4b79e3d749dc2275d4daa456edaee984d
chebykin1993 <chebykin1993@list.ru>
2023-05-07 16:37:08 +0300

    feat: task 4 completed

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
+const promises = require('fs/promises')
+const fs = require("fs")
 
+function copyDir() {
+
+  promises.mkdir("./04-copy-directory/files-copy", {recursive: true})
+  let files = promises.readdir("./04-copy-directory/files",
+      {withFileTypes: true})
+  files.then(files => {
+    files.forEach(file => {
+          promises.copyFile(`./04-copy-directory/files/${file.name}`,
+              `./04-copy-directory/files-copy/${file.name}`)
+        })
+      }
+  )
+    let filesCopy = promises.readdir("./04-copy-directory/files-copy",
+      {withFileTypes: true})
+  filesCopy.then(files => {
+    files.forEach(file => {
+         fs.stat(`./04-copy-directory/files/${file.name}`,
+        (err) => {
+             if (err) {promises.rm(`./04-copy-directory/files-copy/${file.name}`)}
+          })
+        })
+      }
+  )
+    console.log("Procedure completed")
+}
+copyDir()
