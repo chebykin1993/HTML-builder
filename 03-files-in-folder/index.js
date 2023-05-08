907a638150f0f9548da41567127ef99333305141
chebykin1993 <chebykin1993@list.ru>
2023-05-08 14:26:09 +0300

    refactor: edit output text

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -10,7 +10,8 @@ files.then(files => {
           let ext = path.extname(`./03-files-in-folder/secret-folder/${name}`)
           fs.stat(`./03-files-in-folder/secret-folder/${name}`,
               (err, file) =>
-                  console.log(name.slice(0, name.indexOf(".")) + " - " + ext + " - " + file.size))
+                  console.log(name.slice(0, name.indexOf("."))
+                      + " - " + ext.slice(1) + " - " + file.size + 'b'))
       }
   }
 })
